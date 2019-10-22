0511ee5aa0ef90bc7c398e3ad75057529953bd57
German <german_bnf@hotmail.com>
2019-10-22 15:21:18 +0200

    first

---
+++ app/src/main/java/com/example/farmogoapp/ui/main/Unregistercow.java
@@ -0,0 +1,14 @@
+package com.example.farmogoapp.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Unregistercow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unregistercow);
+    }
+}
