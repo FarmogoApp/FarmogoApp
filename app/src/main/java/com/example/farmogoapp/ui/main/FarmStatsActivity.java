e6921f5b4ff2b38268ec489f2dfd1ac3045baf5a
josemanuel97 <manoloainsa@gmail.com>
2019-10-24 13:19:22 +0200

    Create spinner in farm_statistic #39 to choose explotation

--- app/src/main/java/com/example/farmogoapp/ui/main/FarmStatsActivity.java
+++ app/src/main/java/com/example/farmogoapp/ui/main/FarmStatsActivity.java
@@ -2,10 +2,17 @@ package com.example.farmogoapp.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +29,7 @@ public class FarmStatsActivity extends AppCompatActivity {
     private Button searchButton;
     private ImageButton addExplotation;
     private ImageButton modifyExplotation;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,6 +47,7 @@ public class FarmStatsActivity extends AppCompatActivity {
         searchButton = findViewById(R.id.search);
         addExplotation = findViewById(R.id.addExplotation);
         modifyExplotation = findViewById(R.id.modifyExplotation);
+        spinner= findViewById(R.id.spinnerstatistics);
     }
 
     private void registerListeners() {
@@ -58,12 +67,29 @@ public class FarmStatsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         modifyExplotation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FarmStatsActivity.this, AddExploitationActivity.class);
                 startActivity(intent);
             }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //int item = parent.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(), "Explotation:" + parent.getItemAtPosition(position).toString() , Toast.LENGTH_LONG).show();
+
+                }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
         });
     }
 
@@ -72,6 +98,11 @@ public class FarmStatsActivity extends AppCompatActivity {
         yougerCowsTextView.setText(String.valueOf(r.nextInt(100)));
         cowsTextView.setText(String.valueOf(r.nextInt(100)));
         bullsTextView.setText(String.valueOf(r.nextInt(100)));
+
+        String[] incidences =new String[]{"59446556","59846465"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, incidences);
+        spinner.setAdapter(adapter);
+        registerListeners();
     }
 
 }
