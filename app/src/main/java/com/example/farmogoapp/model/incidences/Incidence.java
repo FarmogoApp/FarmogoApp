003ca1d8ce9665c1590f7c050d8de6c80b60a35f
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-12-07 10:18:39 +0100

    Added indidence model

---
+++ app/src/main/java/com/example/farmogoapp/model/incidences/Incidence.java
@@ -0,0 +1,148 @@
+package com.example.farmogoapp.model.incidences;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.util.Date;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = IncidenceWeight.class, name = "WEIGHT"),
+        @JsonSubTypes.Type(value = IncidenceTreatment.class, name = "TREATMENT"),
+        @JsonSubTypes.Type(value = IncidencePregnancy.class, name = "PREGNANCY"),
+        @JsonSubTypes.Type(value = IncidenceDischarge.class, name = "DISCHARGE"),
+        @JsonSubTypes.Type(value = IncidenceDischarge.class, name = "BIRTH")
+})
+public abstract class Incidence {
+    private String uuid;
+    private IncidenceType type;
+    private String observations;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date date;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date dueDate;
+    private boolean done;
+    private boolean complete;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss.S")
+    private Date created;
+    private String createdBy;
+    private String animalId;
+    private String farmId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date removeDate;
+    private String removeReason;
+
+    public Incidence(IncidenceType incidenceType) {
+        type = incidenceType;
+        created = new Date();
+        date = new Date();
+    }
+
+    public IncidenceType getType() {
+        return type;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(String animalId) {
+        this.animalId = animalId;
+    }
+
+    public String getFarmId() {
+        return farmId;
+    }
+
+    public void setFarmId(String farmId) {
+        this.farmId = farmId;
+    }
+
+    public Date getRemoveDate() {
+        return removeDate;
+    }
+
+    public void setRemoveDate(Date removeDate) {
+        this.removeDate = removeDate;
+    }
+
+    public String getRemoveReason() {
+        return removeReason;
+    }
+
+    public void setRemoveReason(String removeReason) {
+        this.removeReason = removeReason;
+    }
+
+    public abstract void accept(IncidenceVisitor visitor);
+}
---
+++ app/src/main/java/com/example/farmogoapp/model/incidences/IncidenceDischarge.java
@@ -0,0 +1,34 @@
+package com.example.farmogoapp.model.incidences;
+
+
+public class IncidenceDischarge extends Incidence {
+
+    private DischargeType dischargeType;
+    private String healthRegister;
+
+    public IncidenceDischarge() {
+        super(IncidenceType.DISCHARGE);
+    }
+
+    public DischargeType getDischargeType() {
+        return dischargeType;
+    }
+
+    public void setDischargeType(DischargeType dischargeType) {
+        this.dischargeType = dischargeType;
+    }
+
+    public String getHealthRegister() {
+        return healthRegister;
+    }
+
+    public void setHealthRegister(String healthRegister) {
+        this.healthRegister = healthRegister;
+    }
+
+
+    @Override
+    public void accept(IncidenceVisitor visitor) {
+        visitor.visit(this);
+    }
+}
