f36ca076bb94d9fa2995cac9eb51fd5a75fa5265
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-12-08 18:57:38 +0100

    Read nfc and open animal info, get animal info from rest and update some fileds.

--- app/src/main/java/com/example/farmogoapp/io/FarmogoApiService.java
+++ app/src/main/java/com/example/farmogoapp/io/FarmogoApiService.java
@@ -3,15 +3,15 @@ package com.example.farmogoapp.io;
 import com.example.farmogoapp.io.response.AnimalTypes;
 import com.example.farmogoapp.io.response.User;
 import com.example.farmogoapp.io.response.Farms;
+import com.example.farmogoapp.model.Animal;
 import com.example.farmogoapp.model.incidences.Incidence;
-
-
 
 import java.util.ArrayList;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface FarmogoApiService {
     @GET("animalTypes")
@@ -25,5 +25,9 @@ public interface FarmogoApiService {
 
     @GET("incidences")
     Call<ArrayList<Incidence>> getIncidences();
+
+    @GET("animals/{id}")
+    Call<Animal> getAnimal(@Path("id") String idAnimal);
+
 }
 
