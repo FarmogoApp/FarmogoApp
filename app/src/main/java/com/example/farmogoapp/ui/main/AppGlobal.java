31aab03d65ac7eba9a54691f44d15d715ec8e10e
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-12-10 19:32:40 +0100

    Implemented basic cache system.

---
+++ app/src/main/java/com/example/farmogoapp/ui/main/AppGlobal.java
@@ -0,0 +1,22 @@
+package com.example.farmogoapp.ui.main;
+
+
+
+import android.app.Application;
+
+public class AppGlobal extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CacheManager.init(this);
+    }
+
+
+
+    @Override
+    public void onTerminate() {
+        CacheManager.getInstance().shutdown();
+        super.onTerminate();
+    }
+}
